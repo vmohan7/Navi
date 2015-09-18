4198de43d66e6ff601ad147fbc647755a256d2a1
Vasanth Mohan <vmohan7@stanford.edu>
2015-09-17 19:23:50 -0700

    Added support for device orientation on reset. Will push build to app store in a few days

--- NaviMobile/Assets/NaviMobileScripts/GyroManager.cs
+++ NaviMobile/Assets/NaviMobileScripts/GyroManager.cs
@@ -24,8 +24,13 @@ using System.Collections;
 ///  device. In this case, it uses the gyro to set rotational data. 
 /// </summary>
 public class GyroManager : TransformManagerInterface {
+	
+	private const float ScreenRotationThreshold = 0.7f; //Accelerometer threshold for switching orientation on reset
 
+	private bool isUpsideDown = false; //Boolean to detech if we are using upside down portrait, which requires a slightly different use of gyrp
+	private bool isReseting = false; //avoid multiple reset commands interfering with each other
 	private Quaternion initalRotation; //rotation of last reset
+
 
 	/// <summary>
 	///  Method to initalize the gyro at start of game
@@ -34,17 +39,18 @@ public class GyroManager : TransformManagerInterface {
 		// Activate the gyroscope
 		Input.gyro.enabled = true;
 		initalRotation = Quaternion.identity;
+
 	}
 	
 	/// <summary>
 	///  Gets gyro data and uses it to update rotation relative to reset position
 	/// </summary>
 	void Update () {
-		//transform.Rotate (-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, Input.gyro.rotationRateUnbiased.z);
-
-		Quaternion tempRot = Quaternion.identity;
-		ComputeRotation (out tempRot);
-		transform.rotation = Quaternion.Inverse (initalRotation) * tempRot;
+		if (!isReseting) { //only change orientation if we are not reseting
+			Quaternion tempRot = Quaternion.identity;
+			ComputeRotation (out tempRot);
+			transform.rotation = Quaternion.Inverse (initalRotation) * tempRot;
+		}
 	}
 
 	/// <summary>
@@ -53,14 +59,48 @@ public class GyroManager : TransformManagerInterface {
 	/// <param name="rot">The output rotation from this calculation</param> 
 	private void ComputeRotation(out Quaternion rot){
 		var att = Input.gyro.attitude;
-		att = new Quaternion(att.x, att.y, -att.z, -att.w);
-		rot = Quaternion.Euler(90, 0, 0) * att;
+		if (isUpsideDown) {
+			att = new Quaternion(att.x, att.y, att.z, att.w);
+		} else {
+			att = new Quaternion(att.x, att.y, -att.z, -att.w);
+		}
+
+		rot = Quaternion.Euler(90f, 0f, 0f) * att;
 	}
 
 	/// <summary>
 	///  How to reset the position of the device
 	/// </summary>
 	public override void Reset(){
-		ComputeRotation (out initalRotation);
+		StartCoroutine (ResetOrientation ()); //start a courtine to apply reset settings
 	}
+
+	/// <summary>
+	///  Couroutine to apply approraite wait times for calibrating device and screen orientation
+	/// </summary>
+	IEnumerator ResetOrientation() {
+		if (!isReseting) { //only reset if no other coroutine is reseting
+			isReseting = true;
+			Input.compensateSensors = false; //do not compensate for orientation, so that we get raw reading from accelerometer
+			yield return new WaitForSeconds(.05f); //wait a few frames for a fresh reading
+			isUpsideDown = false;
+			if (Input.acceleration.x < -ScreenRotationThreshold) {
+				Screen.orientation = ScreenOrientation.LandscapeLeft;
+			} else if (Input.acceleration.y < -ScreenRotationThreshold) {
+				Screen.orientation = ScreenOrientation.Portrait;
+			} else if (Input.acceleration.y > ScreenRotationThreshold) {
+				Screen.orientation = ScreenOrientation.PortraitUpsideDown;
+				isUpsideDown = true;
+			} else if (Input.acceleration.x > ScreenRotationThreshold) {
+				Screen.orientation = ScreenOrientation.LandscapeRight;
+			}
+			yield return new WaitForSeconds(.1f); //wait for device to switch orientations
+			Input.compensateSensors = true; //recompensate for orientation for the gyro orientation
+			yield return new WaitForSeconds(.05f); //wait a few frames for a fresh reading
+
+			ComputeRotation (out initalRotation); //used to reset inital position to be right in front of the camera  
+			isReseting = false; //done reseting
+		}
+	}
+
 }
