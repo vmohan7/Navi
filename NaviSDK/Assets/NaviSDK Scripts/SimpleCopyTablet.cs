5fedf9977c3af81ce298f4c2e298cb8b101757e0
Vasanth Mohan <vmohan7@stanford.edu>
2015-09-23 19:56:26 -0700

    Add SimpleCopyTablet Script and SDK build

---
+++ NaviSDK/Assets/NaviSDK Scripts/SimpleCopyTablet.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleCopyTablet : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = NaviDeviceLocation.DeviceLocation.transform.rotation;
+	}
+}
